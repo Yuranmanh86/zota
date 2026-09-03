34a92fd12bde051f7e55a58bf8aa50ac10e0dee8
yuranmanh86 <dossantosyuran30@gmsil.com>
2026-09-03 14:09:46 +0200

    build: publish higher install button

--- App.js
+++ App.js
@@ -228,7 +228,7 @@ const webShellStyles = StyleSheet.create({
   },
   installButton: {
     position: 'absolute',
-    bottom: 150,
+    bottom: 190,
     right: 12,
     zIndex: 10000,
     flexDirection: 'row',
